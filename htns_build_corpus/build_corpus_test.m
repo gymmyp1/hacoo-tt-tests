cab47898b1a09555b484ff648e5ce24f33bfe0ff
gymmyp1 <40799820+gymmyp1@users.noreply.github.com>
2023-01-02 15:40:28 -0500

    Add files via upload

---
+++ htns_build_corpus/build_corpus_test.m
@@ -0,0 +1,10 @@
+%Build tensors from a directory of .txt files
+
+addpath  C:\Users\MeiLi\OneDrive\Documents\MATLAB
+%addpath /Users/meilicharles/Documents/MATLAB/hacoo-matlab/
+
+tic
+
+doctns()
+
+toc
---
+++ small_build_corpus/run.m
@@ -0,0 +1,7 @@
+%Build tensors from a directory of .txt files using HaCOO structure.
+
+addpath  C:\Users\MeiLi\OneDrive\Documents\MATLAB
+%addpath /Users/meilicharles/Documents/MATLAB/hacoo-matlab/
+
+f = @() tt_doctns(); % handle to function
+timeit(f)
